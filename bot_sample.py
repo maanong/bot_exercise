fbf793b37613301b31eedd0319a8b71a077f38cd
maanong <58322744+maanong@users.noreply.github.com>
2024-07-12 17:21:38 +0900

    Update bot_sample.py
    일단 프로토타입에 필요한 내용들 적어놓음 ㅇㅇ

--- bot_sample.py
+++ bot_sample.py
@@ -1,3 +1,7 @@
+# 중요!
+## 이 봇은 어디까지나 제작할 봇에 대한 프로토타입임 일단은 디스코드 봇으로 하는 것으로 방향을 잡았기에 디스코드 봇으로 올리지만 어디까지나 다른 플랫폼 봇으로 사용할 수도 있음
+## 우선은 봇에 대한 잡기능을 이것 저것 넣어놓았음 ㅇㅇ 기초적인 뼈대 같은 거임 추후에 잡기능 중에 마음에 안드는 거 있으면 치울거임
+
 import discord
 from discord.ext import commands
 import random
@@ -13,6 +17,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
+# 특정 채팅에 실행되는 이벤트
 @bot.event
 async def on_message(message):
     if message.author.bot:
@@ -49,8 +54,10 @@ async def on_message(message):
         
     await bot.process_commands(message)
 
-
+# 여기서부터는 명령어를 입력해야 실행되는 이벤트 명령어는 !(명령어)식으로 입력하면 됨
 @bot.command()
+
+# 주의! 이건 샘플을 보여주자면 !선택 가위 이런 식으로 명령어를 입력해줘야함
 async def 선택(ctx,usr_choice:str):
     choices = ['가위','바위','보']
     bot_choice = random.choice(choices)
@@ -93,7 +100,10 @@ async def 로또(ctx):
 async def 훈수(ctx):
     await ctx.send('아 그거 그렇게 하는 거 아닌데')
     ## 여기다가 해당 내용에 대한 API나 검색엔진을 활용해서 크롤링하여 훈수를 두는 형식으로 작동
-    ### input your code!
+    ## TTS 추가 필요 음성 통화 채널에서도 동작할 필요성이 있음
+    ## 훈수를 두는 형식은 다음과 같음 만일 롤을 하는 경우 픽을 골랐을 때 그 픽이 승률이 구린 픽이라면 그거 똥챔인데 이런 식으로 TTS로 이야기 하도록 할 것!
+    ## 아이템의 경우 첫 템이 도란의 반지인 경우 TTS로 아 그거 도란링 보다 도란방패가 더 좋은데 이런 식으로 이야기를 할 것
+    ### input your code! ###
 
 # 디스코드 봇 토큰
 TOKEN = 'INPUT TOKEN!!!!!'
